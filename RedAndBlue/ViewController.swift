a17c907eae0719f6a21a3164b640d81734567e82
Hector Rios <oj.rios@gmail.com>
2016-02-03 22:13:58 +0100

    Devslopes 1st Practice App. Hiding images

--- RedAndBlue/ViewController.swift
+++ RedAndBlue/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bluePacifier: UIImageView!
+    @IBOutlet weak var redPacifier: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,12 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func hideRedPacifierBtnClicked(sender: AnyObject) {
+        self.redPacifier.hidden = true
+    }
+    
+    @IBAction func hideBluePacifierBtnClicked(sender: AnyObject) {
+        self.bluePacifier.hidden = true
+    }
 }
 
